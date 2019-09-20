738619eace33e66de7a7d0b4a23e6411d33bf69a
Ethan Sternke <ethan.sternke@gmail.com>
2019-09-20 10:18:31 -0400

    [DOX-5340] Multipart PUT request

--- lib/docusign_rest/client.rb
+++ lib/docusign_rest/client.rb
@@ -603,6 +603,16 @@ module DocusignRest
       request
     end
 
+    def initialize_net_http_multipart_put_request(uri, post_body, file_params, headers)
+      request = Net::HTTP::Put::Multipart.new(
+        uri.request_uri,
+        { post_body: post_body }.merge(file_params),
+        headers
+      )
+      debugger
+      request.body = request.body_stream.read
+      request
+    end
 
     # Public: creates an envelope from a document directly without a template
     #
@@ -671,12 +681,6 @@ module DocusignRest
       ios = create_file_ios(options[:files])
       file_params = create_file_params(ios)
 
-      options[:content_type] ||= 'application/json'
-
-      headers = {
-        'Content-Type' => options[:content_type],
-      }
-
       post_body = {
         emailBlurb:   "#{options[:email][:body] if options[:email]}",
         emailSubject: "#{options[:email][:subject] if options[:email]}",
@@ -691,14 +695,13 @@ module DocusignRest
         customFields: options[:custom_fields]
       }.to_json
       
-      post_body.merge(file_params)
-
       uri = build_uri("/accounts/#{@acct_id}/envelopes/#{options[:envelope_id]}/documents")
 
       http = initialize_net_http_ssl(uri)
-      
-      request = Net::HTTP::Put.new(uri.request_uri, headers(headers))
-      request.body = post_body
+
+      request = initialize_net_http_multipart_put_request(
+        uri, post_body, file_params, headers(options[:headers])
+      )
       
       response = http.request(request)
       JSON.parse(response.body)
